6cfdd75e5926a09d5261115cc49556edda47724d
Ashwin901 <ashwinprasanna9@gmail.com>
2021-08-28 18:43:18 +0530

    configure sql cloud server

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const requestLogger = require("./middleware/request_logger");
+const { DATABASE_HOST, DATABASE_DB, DATABASE_PASSWORD, DATABASE_USER } = require("./config");
 const app = express();
+
+var mysql = require('mysql');
+
+try {
+    var connection = mysql.createConnection({
+        host: DATABASE_HOST,
+        user: DATABASE_USER,
+        password: DATABASE_PASSWORD,
+        database: DATABASE_DB
+    });
+
+    connection.connect();
+} catch (e) {
+    console.log(e);
+}
+
+
+connection.end();
 
 app.use(express.json());
 app.use(requestLogger);
--- config.js
+++ config.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT;
+const DATABASE_HOST = process.env.DATABASE_HOST;
+const DATABASE_DB = process.env.DATABASE_DB;
+const DATABASE_USER = process.env.DATABASE_USER;
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 
 module.exports = {
-    PORT
+    PORT,
+    DATABASE_HOST,
+    DATABASE_DB,
+    DATABASE_USER,
+    DATABASE_PASSWORD
 };
