93df316819c2e7977218c0562c57abbc642cb76e
Ashwin901 <ashwinprasanna9@gmail.com>
2021-09-21 16:27:52 +0530

    add mongodb database

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const requestLogger = require("./middleware/request_logger");
-const { DATABASE_HOST, DATABASE_DB, DATABASE_PASSWORD, DATABASE_USER } = require("./config");
+const db = require("./services/db_connect");
 const app = express();
-
-var mysql = require('mysql');
-
-try {
-    var connection = mysql.createConnection({
-        host: DATABASE_HOST,
-        user: DATABASE_USER,
-        password: DATABASE_PASSWORD,
-        database: DATABASE_DB
-    });
-
-    connection.connect();
-} catch (e) {
-    console.log(e);
-}
-
-
-connection.end();
 
 app.use(express.json());
 app.use(requestLogger);
--- config.js
+++ config.js
@@ -1,15 +1,9 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT;
-const DATABASE_HOST = process.env.DATABASE_HOST;
-const DATABASE_DB = process.env.DATABASE_DB;
-const DATABASE_USER = process.env.DATABASE_USER;
-const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 module.exports = {
     PORT,
-    DATABASE_HOST,
-    DATABASE_DB,
-    DATABASE_USER,
-    DATABASE_PASSWORD
+    MONGODB_URI
 };
