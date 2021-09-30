8bc6f078356059324872bfae716baaee1647d5e7
rahulkr66 <rahulkr.191cs239@nitk.edu.in>
2021-09-30 19:36:38 +0530

    added basic user login and user registration with basic user schema
    Signed-off-by: rahulkr66 <rahulkr.191cs239@nitk.edu.in>

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const cors = require("cors");
 const requestLogger = require("./middleware/request_logger");
 const db = require("./services/database");
 const AuthController = require("./controllers/auth_controller");
+
+
 const app = express();
+
 
 app.use(express.json());
 app.use(requestLogger);
 app.use(cors());
+app.use(express.urlencoded({
+    extended: true
+}))
 
 app.get("/api", (req, res) => {
     res.send("Hello world!");
--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -3,8 +3,9 @@ const AuthController = express.Router();
 const bcrypt = require("bcryptjs");
 const { generateToken } = require("../services/authentication");
 const Organization = require("../models/organization");
+const User = require('../models/user');
 
-AuthController.post("/register", (req, res) => {
+AuthController.post("/org/register", (req, res) => {
     const organizationName = req.body.organizationName;
     const organizationEmail = req.body.organizationEmail;
     const password = req.body.password;
@@ -45,7 +46,7 @@ AuthController.post("/register", (req, res) => {
     );
 });
 
-AuthController.post("/login", async (req, res) => {
+AuthController.post("/org/login", async (req, res) => {
     const organizationEmail = req.body.organizationEmail;
     const password = req.body.password;
 
@@ -86,5 +87,71 @@ AuthController.post("/login", async (req, res) => {
         }
     }
 });
+
+//user schema : userName, userEmail, password
+
+AuthController.post('/user/register', async (req, res) => {
+    const userName = req.body.userName
+    const userEmail = req.body.userEmail
+    const password = req.body.password
+
+    const hashedPassword = bcrypt.hashSync(password, 8)
+    User.create({
+        userName: userName,
+        userEmail: userEmail,
+        password: hashedPassword,
+    }, (e, user) => {
+        if (e) {
+            const message = e.code == 11000 ? "User already registered" : "Try again later"
+            return res.status(500).json({
+                auth: false,
+                message : message
+            })
+        }
+        const token = generateToken(user._id)
+        res.status(200).json({
+            auth: true,
+            token: token,
+            userId: user._id,
+            userName: user.name,
+            userEmail: user.email,
+            message: 'Registration success'
+        })
+    })
+})
+
+AuthController.post('/user/login', async (req, res) => {
+    const userEmail = req.body.userEmail
+    const password = req.body.password
+    try {
+        const user = await User.findOne({ email: userEmail })
+        if (!user) {
+            console.error('No user found')
+            return res.status(404).json({
+                auth: false, message: "User not found"
+            })
+        }
+        const checkPassword = bcrypt.compareSync(password, user.password)
+        if (!checkPassword) {
+            console.error('Invalid password')
+            return res.status(401).json({ auth: false, token: null, message: 'Invalid password' })
+        }
+        const token = generateToken(user._id)
+        res.status(200).json({
+            auth: true,
+            token: token,
+            userId: user._id,
+            userName: user.name,
+            userEmail: user.email,
+            message: 'Login successful'
+        })
+    } catch (e) {
+        if (e) {
+            console.error('Login error ' + e)
+            return res.status(500).json({
+                auth: false, token: null,message: 'Login failed. Try again!'})
+        }
+    }
+})
 
 module.exports = AuthController;
